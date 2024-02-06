e6b40e7100652def409b1b741868bbb3900debfa
Neil Daterao <ndaterao@gmail.com>
2024-02-06 10:09:10 -0500

    Initialized new files for menu, score, and wordsfound

--- src/main/java/WordHuntGameApp.java
+++ src/main/java/WordHuntGameApp.java
@@ -1,12 +1,5 @@
 
 public class WordHuntGameApp { 
-    //testing code, delete
-    private int[] list; 
-    public WordHuntGameApp() { 
-        list = new int[4];
-    }
-
-    public void addNum(int num){ 
-        list[0] = num;
-    }
+    
+    
 }
--- src/main/java/WordHuntListener.java
+++ src/main/java/WordHuntListener.java
@@ -1,3 +1,6 @@
 public interface WordHuntListener {
     
+    public void update();
+
+    
 }

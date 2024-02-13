f3229cfa9394d090ff3960afa0b89f73945afaee
Neil Daterao <ndaterao@gmail.com>
2024-02-13 10:12:39 -0500

    New in game menu branch

--- src/main/java/view/WordHuntGameMain.java
+++ src/main/java/view/WordHuntGameMain.java
@@ -14,6 +14,7 @@ public class WordHuntGameMain extends Application {
     private WordHuntMenuViewModel menuViewModel;
     private WordHuntMenu wordHuntMenu;
     
+    
 
     public static void main(String[] args) {
         launch(args);

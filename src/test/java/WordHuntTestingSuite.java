6a36b0604f682a2b72f499346beef4ca0996ceaf
Jacob Schuckman <jacobschuckman@Jacobs-MacBook-Pro-2.local>
2024-02-20 07:52:09 -0500

    fixed testing suite syntax

--- src/test/java/WordHuntTestingSuite.java
+++ src/test/java/WordHuntTestingSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-    WordHuntGameAppTests.class
+    WordHuntGameTests.class
     WordHuntScoreTests.class
     WordHuntWordsTests.class
 })
